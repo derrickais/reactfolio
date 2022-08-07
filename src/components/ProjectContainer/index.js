9411e0dfe2511e1547ef1e770251f3a8708577b7
Derricka S <derrickas728@gmail.com>
2022-08-07 18:35:59 -0400

    adds other projects

--- src/components/ProjectContainer/index.js
+++ src/components/ProjectContainer/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import runBuddy from '../../assets/images/run-buddy.png';
 import nee from '../../assets/images/north-eastern-events.png';
 import nee2 from '../../assets/images/north-eastern-events-express.png';
+import placeholderBanner from '../../assets/images/header-image.jpeg';
+import weatherly from '../../assets/images/weatherly.png'
+import notetaker from '../../assets/images/notetaker.png';
+import workdayScheduler from '../../assets/images/workdayScheduler.png';
 import Project from '../Project';
 
 function ProjectContainer() {
@@ -13,6 +17,18 @@ function ProjectContainer() {
         name: 'North Eastern Events',
         url: 'https://derrickais.github.io/nae-events/',
         image: nee
+    }, {
+        name: 'Weatherly',
+        url: 'https://github.com/derrickais/weatherly',
+        image: weatherly
+    }, {
+        name: 'NoteTaker',
+        url: 'https://github.com/derrickais/note-taker',
+        image: notetaker
+    }, {
+        name: 'Workday Scheduler',
+        url: 'https://github.com/derrickais/workday-scheduler',
+        image: workdayScheduler
     }, {
         name: 'North Eastern Events Express',
         url: 'https://radiant-sierra-52768.herokuapp.com/index.html',
