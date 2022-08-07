8cb1af2620bc4b34c44e8eeb8d06641117d0b824
Derricka S <derrickas728@gmail.com>
2022-08-07 16:20:19 -0400

    sets up initial components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './components/About';
+import ProjectContainer from './components/ProjectContainer'
+import Resume from './components/Resume';
+import Contact from './components/Contact';
+
+const mainViews = {
+  "about": () => <About />,
+  "projects": () => <ProjectContainer />,
+  "contact": () => <Contact />,
+  "resume": () => <Resume />
+}
 
 function App() {
+  const [route, setRoute] = useState('about')
+
+  const activeView = mainViews[route]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+        {activeView()}
+      <Footer />
     </div>
   );
 }
